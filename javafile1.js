358bbc8af4642da2b400f69cc9e30e5a2bf81f99
abbasskak <63091984+abbasskak@users.noreply.github.com>
2022-10-12 19:44:17 +1100

    Update javafile1.js

--- javafile1.js
+++ javafile1.js
@@ -1,8 +1,6 @@
 var fg_1 = "FirstGraph.json";
 vegaEmbed("#FIRSTGRAPH", fg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var fg_2 = "Latlongchart.json";
 vegaEmbed("#SECONDGRAPH", fg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
