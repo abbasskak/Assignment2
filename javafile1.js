fae5b2952c3021299cd677c63e2296c60a1e9eb4
abbasskak <63091984+abbasskak@users.noreply.github.com>
2022-10-12 13:41:35 +1100

    Add files via upload

---
+++ javafile1.js
@@ -0,0 +1,5 @@
+var fg_1 = "https://raw.githubusercontent.com/abbasskak/Assignment2/main/FirstGraph.json";
+vegaEmbed("#FIRSTGRAPH", fg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
