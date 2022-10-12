b30570eebb550fd4d09d2fcb9eaf763bf237cdd2
abbasskak <63091984+abbasskak@users.noreply.github.com>
2022-10-12 14:00:34 +1100

    Update javafile1.js

--- javafile1.js
+++ javafile1.js
@@ -1,4 +1,4 @@
-var fg_1 = "https://raw.githubusercontent.com/abbasskak/Assignment2/main/FirstGraph.json";
+var fg_1 = "FirstGraph.json";
 vegaEmbed("#FIRSTGRAPH", fg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
